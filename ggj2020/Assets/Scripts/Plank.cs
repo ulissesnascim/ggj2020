0c8ab9e6508017fb4ffd7352c1e7d44917def9b2
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2020-02-01 03:36:36 -0300

    pequenos ajustes no script de spawn e da plank

--- ggj2020/Assets/Scripts/Plank.cs
+++ ggj2020/Assets/Scripts/Plank.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Plank : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 0;
+    private float moveSpeed;
 
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-    }
+        // Randomiza uma velocidade para a plank entre 3 e 5 (inteiros)
+        moveSpeed = Random.Range(3, 6);
+        Destroy(gameObject, 5f);
+   }
 
     private void FixedUpdate()
     {
         rb.velocity = transform.right * moveSpeed;
-
     }
 
 }
