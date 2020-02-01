d7cc989e2bcf8ccde1970bc6cf2aaebba4bcca72
Ulisses <ulisse@painfulsmile.com.br>
2020-02-01 02:15:34 -0300

    Setup inicial. Spawn dos pallets

---
+++ ggj2020/Assets/Scripts/Plank.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plank : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.forward * moveSpeed;
+
+    }
+
+}
---
+++ ggj2020/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private Transform leftSpawnPoint = null;
+    [SerializeField] private Transform rightSpawnPoint = null;
+    [SerializeField] private float minTimeBetweenSpawns = 0;
+    [SerializeField] private float maxTimeBetweenSpawns = 0;
+    [SerializeField] private GameObject plank = null;
+
+    private float spawnTimerLeft = 0;
+    private float spawnTimerRight = 0;
+
+    private float randomSpawnTimeLeft = 0;
+    private float randomSpawnTimeRight = 0;
+
+    private void Start()
+    {
+        SetRandomSpawnTimeLeft();
+        SetRandomSpawnTimeRight();
+    }
+
+    private void Update()
+    {
+        spawnTimerLeft += Time.deltaTime;
+        spawnTimerRight += Time.deltaTime;
+
+        if (spawnTimerLeft > randomSpawnTimeLeft)
+        {
+            SpawnLeft();
+        }
+
+        if (spawnTimerRight > randomSpawnTimeRight)
+        {
+            SpawnRight();
+        }
+
+    }
+
+    private void SetRandomSpawnTimeLeft()
+    {
+        randomSpawnTimeLeft = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+    }
+
+    private void SetRandomSpawnTimeRight()
+    {
+        randomSpawnTimeRight = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+    }
+
+    private void SpawnLeft()
+    {
+        Instantiate(plank, leftSpawnPoint.position, leftSpawnPoint.rotation);
+
+        spawnTimerLeft = 0;
+    }
+
+    private void SpawnRight()
+    {
+        Instantiate(plank, rightSpawnPoint.position, rightSpawnPoint.rotation);
+
+        spawnTimerRight = 0;
+    }
+}
