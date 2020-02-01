aadb1a827ada20dd0ff047fea2d57338971132dc
Ulisses <ulisse@painfulsmile.com.br>
2020-02-01 03:11:28 -0300

    Implementa spawn de objetos aleatórios

--- ggj2020/Assets/Scripts/SpawnManager.cs
+++ ggj2020/Assets/Scripts/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private Transform rightSpawnPoint = null;
     [SerializeField] private float minTimeBetweenSpawns = 0;
     [SerializeField] private float maxTimeBetweenSpawns = 0;
-    [SerializeField] private GameObject plank = null;
+    [SerializeField] private List<GameObject> grabbableObjects = new List<GameObject>();
 
     private float spawnTimerLeft = 0;
     private float spawnTimerRight = 0;
@@ -51,15 +51,24 @@ public class SpawnManager : MonoBehaviour
 
     private void SpawnLeft()
     {
-        Instantiate(plank, leftSpawnPoint.position, leftSpawnPoint.rotation);
+        Instantiate(RandomGrabbableObject(), leftSpawnPoint.position, Quaternion.identity);
 
         spawnTimerLeft = 0;
+        SetRandomSpawnTimeLeft();
     }
 
     private void SpawnRight()
     {
-        Instantiate(plank, rightSpawnPoint.position, rightSpawnPoint.rotation);
+        Instantiate(RandomGrabbableObject(), rightSpawnPoint.position, Quaternion.identity);
 
         spawnTimerRight = 0;
+        SetRandomSpawnTimeRight();
+    }
+
+    private GameObject RandomGrabbableObject()
+    {
+        int index = Random.Range(0, grabbableObjects.Count - 1);
+        return grabbableObjects[index];
+
     }
 }
