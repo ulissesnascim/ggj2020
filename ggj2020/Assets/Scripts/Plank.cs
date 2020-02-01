4ec5ec7a893e08dfa4e4e5e27ddbbf1cdaab1f5d
Ulisses <ulissesnascim@gmail.com>
2020-02-01 15:48:23 -0300

    Corrige mais alguns bugs pequenos

--- ggj2020/Assets/Scripts/Plank.cs
+++ ggj2020/Assets/Scripts/Plank.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class Plank : MonoBehaviour
 {
+    public float minSpeed;
+    public float maxSpeed;
     public Transform ItemInstanceParent;
     [Space(2.5f)]
     public List<GameObject> Itens;
     
     private float _moveSpeed;
-    private Rigidbody _rb;
+    //private Rigidbody _rb;
 
     private void Start()
     {
-        _rb = GetComponent<Rigidbody>();
-        _moveSpeed = Random.Range(3, 6);
+        //_rb = GetComponent<Rigidbody>();
+        _moveSpeed = Random.Range(minSpeed, maxSpeed);
         SpawnItem();
         
-        Destroy(transform.parent.gameObject, 5f);
+        Destroy(gameObject, 5f);
    }
 
     private void FixedUpdate()
     {
-        _rb.velocity = -1 * transform.forward * _moveSpeed;
+        transform.Translate(-1 * transform.forward * _moveSpeed);
+        //_rb.velocity = -1 * transform.forward * _moveSpeed;
     }
 
     private void SpawnItem() 
