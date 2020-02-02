eaffd08275098a17e48c7df199244c1344f9e21d
Ulisses <ulisse@painfulsmile.com.br>
2020-02-02 18:33:13 -0300

    Corrige animação do background e retoma menu estático (vai ficar estático mesmo)

--- ggj2020/Assets/Scripts/MainMenu.cs
+++ ggj2020/Assets/Scripts/MainMenu.cs
@@ -5,47 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private RectTransform playButton = null;
-    private float randomMoveSpeedPlay;
-
-    [SerializeField] private RectTransform quitButton = null;
-    private float randomMoveSpeedQuit;
-
-    [SerializeField] private float minMoveSpeed = 0;
-    [SerializeField] private float maxMoveSpeed = 0;
 
     private void Start()
     {
-        UpdatePlaySpeed();
-        UpdateQuitSpeed();
     }
 
     public void FixedUpdate()
     {
-        MoveRight(playButton, randomMoveSpeedPlay);
-        MoveRight(quitButton, randomMoveSpeedQuit);
-    
     }
-
-    private void MoveRight(RectTransform buttonRect, float moveSpeed)
-    {
-        Vector3 initialPosition = buttonRect.position;
-        buttonRect.position = new Vector3(initialPosition.x + moveSpeed / 100f, initialPosition.y, initialPosition.z);
-
-    }
-
-    private void UpdatePlaySpeed()
-    {
-        randomMoveSpeedPlay = Random.Range(minMoveSpeed, maxMoveSpeed);
-
-    }
-    
-    private void UpdateQuitSpeed()
-    {
-        randomMoveSpeedQuit = Random.Range(minMoveSpeed, maxMoveSpeed);
-
-    }
-
 
     public void LoadScene(string sceneToLoad)
     {
