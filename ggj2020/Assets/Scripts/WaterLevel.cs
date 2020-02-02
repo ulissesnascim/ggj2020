5a396b10ee28f4b30ddcc2b9ce62d6a33274a892
Gabriel R <gabriel_ribeiro@id.uff.br>
2020-02-02 17:31:31 -0300

    Balde implementado. Posso testar @Thiago?

--- ggj2020/Assets/Scripts/WaterLevel.cs
+++ ggj2020/Assets/Scripts/WaterLevel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WaterLevel : MonoBehaviour
 {
-    [HideInInspector] public float CurrentWaterLevel;
+    private float CurrentWaterLevel;
     public float MaximumWaterHeight = 0.75f;
     public float MaximumWaterLevel;
 
@@ -12,10 +12,30 @@ public class WaterLevel : MonoBehaviour
     private float _waterGrowthRate;
     private bool _hasWaterReachedTheMaximunLevel = false;
 
+    public static WaterLevel instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
     private void Start()
     {
         CurrentWaterLevel = 0;
         _waterGrowthRate = MaximumWaterHeight / MaximumWaterLevel;
+    }
+
+    public void AddWater(float amount)
+    {
+        CurrentWaterLevel += amount;
+    }
+
+    public void RemoveWater(float amount)
+    {
+        CurrentWaterLevel -= amount;
     }
 
     private void Update()
