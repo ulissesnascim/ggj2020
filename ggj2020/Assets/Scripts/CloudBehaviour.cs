4f94ecfca0d705b68ce452b112d3fae8214786fd
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2020-02-03 00:32:18 -0300

    Nuvens

--- ggj2020/Assets/Scripts/CloudBehaviour.cs
+++ ggj2020/Assets/Scripts/CloudBehaviour.cs
@@ -1,23 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
-public class SharkBehaviour : MonoBehaviour
+public class CloudBehaviour : MonoBehaviour
 {
     public float velocity;
-    public Transform VFX;
+    public float TimeToChangeDirection = 10f;
 
     private int direction = 1;
     private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("ChangeDirection", 0f, 6f);      
+        InvokeRepeating("ChangeDirection", 0f, TimeToChangeDirection);
         rb = GetComponent<Rigidbody>();
     }
 
-    private void ChangeDirection() 
+    private void ChangeDirection()
     {
         direction = direction * -1;
         rb.velocity = transform.forward * velocity * direction;
