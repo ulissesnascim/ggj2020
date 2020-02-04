f692f4d9813ef982ef4043b08f52e8bb84752a29
Ulisses <ulissesnascim@gmail.com>
2020-02-04 04:03:22 -0300

    Melhoria nos tamanhos de buracos, clean up de algumas coisas

--- ggj2020/Assets/Scripts/BoatController.cs
+++ ggj2020/Assets/Scripts/BoatController.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour
 {
-    public List<GameObject> HolesPrefabs;
-    public GameObject HolesPlane;
-    
     void Start()
     {
-        //InstantiateHole();
     }
 
-    private void InstantiateHole() 
-    {
-        int r = Random.Range(0, HolesPrefabs.Count);
-
-        GameObject hole = Instantiate(HolesPrefabs[r], HolesPlane.transform.position, Quaternion.identity);
-    }
 }
