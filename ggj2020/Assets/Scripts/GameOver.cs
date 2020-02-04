235c77c8ff57e8e7dc6891118cb5fe38ed444d9b
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2020-02-03 23:42:11 -0300

    implementação do gameover

--- ggj2020/Assets/Scripts/GameOver.cs
+++ ggj2020/Assets/Scripts/GameOver.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
 public class GameOver : MonoBehaviour
-{
-
-    public Text GameOverText;
+{   
     public Image Background;
-    public Button PlayButton;
-    
+   
     public void GameOverEffect()
     {
         Background.DOColor(Color.black, 2.5f).OnComplete(() => 
         {
-            GameOverText.enabled = true;
-            PlayButton.enabled = true;
+            LevelManager levelManager = FindObjectOfType<LevelManager>();
+
+            levelManager.GoToScene("GameOver");
         });
     }
 }
